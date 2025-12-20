c84620d876d76fdf6d3996121ac2c078b3ff0c27
Drummonds <drummond05@proton.md>
2025-12-20 08:41:49 -0300

    fix: update user data don't invalidade cache properly

--- src/modules/admin/users/hooks/query-keys.ts
+++ src/modules/admin/users/hooks/query-keys.ts
@@ -1,5 +1,7 @@
 export const adminUsersQueryKeys = {
   usersBase: () => ["admin-users-table"] as const,
   getAllUsers: (params?: { search?: string; page?: number; limit?: number }) =>
-    [...adminUsersQueryKeys.usersBase(), params] as const,
+    params
+      ? ([...adminUsersQueryKeys.usersBase(), params] as const)
+      : adminUsersQueryKeys.usersBase(),
 };
