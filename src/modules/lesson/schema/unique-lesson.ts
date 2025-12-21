9599dd083ad94f8da4947732c526d133734d4d82
Drummonds <drummond05@proton.me>
2025-12-21 09:00:33 -0300

    fix: zod throwing erro on completed key

--- src/modules/lesson/schema/unique-lesson.ts
+++ src/modules/lesson/schema/unique-lesson.ts
@@ -5,7 +5,7 @@ import z from "zod";
 export const uniqueLessonSchema = lessonSchema.extend({
   prevLesson: zodSafeString().nullable(),
   nextLesson: zodSafeString().nullable(),
-  completed: zodSafeString().nullable(),
+  completed: z.string(),
   videoUrl: zodSafeString(),
 });
 
